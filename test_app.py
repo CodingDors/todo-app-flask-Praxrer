ab724a3a2bcba98b576c17571503f82260e37291
Dors Coding School <93161768+Dors-Coding-School@users.noreply.github.com>
2023-11-17 12:50:25 -0300

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -10,24 +10,22 @@ class TestToDoApp(unittest.TestCase):
         self.app.testing = True
 
     def test_home_page(self):
-        # sends HTTP GET request to the application
-        # on the specified path
         result = self.app.get('/')
-        
-        # assert the status code of the response
-        self.assertEqual(result.status_code, 200)
-        # assert that the response contains the HTML form
-        self.assertIn(b'Add a New Task', result.data)
+        self.assertEqual(result.status_code, 200, "Home page should load with status code 200")
+        self.assertIn(b'Add a New Task', result.data, "Home page should contain the 'Add a New Task' form")
 
     def test_add_task(self):
-        # sends HTTP POST request to the application
-        # with the specified path and data
         result = self.app.post('/', data=dict(task="Test Task"), follow_redirects=True)
+        self.assertEqual(result.status_code, 200, "Adding a task should redirect to home with status code 200")
+        self.assertIn(b'Test Task', result.data, "The response should contain the added task 'Test Task'")
+
+    def test_remove_task(self):
+        self.app.post('/', data=dict(task="Task to Remove"), follow_redirects=True)
+        remove_result = self.app.post('/remove', data=dict(task="Task to Remove"), follow_redirects=True)
+        self.assertEqual(remove_result.status_code, 200, "Removing a task should redirect to home with status code 200")
         
-        # assert the status code of the response
-        self.assertEqual(result.status_code, 200)
-        # assert that the response contains the new task
-        self.assertIn(b'Test Task', result.data)
+        home_page_result = self.app.get('/')
+        self.assertNotIn(b'Task to Remove', home_page_result.data, "After removal, the task 'Task to Remove' should not be in the response")
 
 # runs the unit tests in the module
 if __name__ == '__main__':
